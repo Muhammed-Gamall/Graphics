2ebd600e3e72e12985cb4f0204e17ab70468ec0d
Muhammed-Gamall <151952009+Muhammed-Gamall@users.noreply.github.com>
2024-01-02 11:40:03 +0200

    Add files via upload

---
+++ Graphics/GraphicsForm.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+using System.Threading;
+namespace Graphics
+{
+    public partial class GraphicsForm : Form
+    {
+        Renderer renderer = new Renderer();
+        
+        public GraphicsForm()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+            initialize();
+            programTimer.Enabled = true;
+        }
+        void initialize()
+        {
+            renderer.Initialize();   
+        }
+        
+        private void GraphicsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            renderer.CleanUp();
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            renderer.Update();
+            renderer.Draw();
+        }
+
+        private void programTimer_Tick(object sender, System.EventArgs e)
+        {
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void simpleOpenGlControl1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            float speed = 5;
+
+            if (e.KeyChar == 'd')
+                renderer.translationX += speed; 
+            if (e.KeyChar == 'a')
+                renderer.translationX -= speed;
+            
+            if (e.KeyChar == 'w')
+                renderer.translationY += speed;
+            if (e.KeyChar == 's')
+                renderer.translationY -= speed;
+
+            if (e.KeyChar == 'z')
+                renderer.translationZ += speed;
+            if (e.KeyChar == 'c')
+                renderer.translationZ -= speed;
+
+
+
+
+            if (e.KeyChar == 'p')
+            {
+                renderer.scalingX *= 1.5f;
+                //renderer.scalingZ *= 1.5f;
+
+            }
+            if (e.KeyChar == 'o')
+                renderer.scalingX /= 1.5f;
+            if (e.KeyChar == 'i')
+                renderer.scalingY *= 1.5f;
+            if (e.KeyChar == 'p')
+                renderer.scalingY /= 1.5f;
+            
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
